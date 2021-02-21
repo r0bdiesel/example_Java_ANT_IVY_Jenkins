d7ad0f11746edacbde48942374213c52d2da205e
r0bdiesel <robertjamesbannister@gmail.com>
2021-02-21 02:24:22 -0500

    Update HelloWorldTest.java

--- tst/test/HelloWorldTest.java
+++ tst/test/HelloWorldTest.java
@@ -1,1 +1,28 @@
-hbfjre
+package test;
+
+import org.junit.Test;
+import org.junit.Ignore;
+import static org.junit.Assert.assertEquals;
+
+
+public class HelloWorldTest{
+  
+  String message = "Robert";	
+   
+   @Test
+   public void testA() {	 
+      assertEquals(message, "Robert");     
+   }
+  
+   @Test
+   public void testA() {	 
+      assertEquals(true, true);     
+   }
+  
+   @Ignore
+   @Test
+   public void testIgnore() {
+      assertEquals("this", "that");
+   }
+  
+}
